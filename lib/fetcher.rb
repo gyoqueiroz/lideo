e828ac7b450bf80f41a8e4b60ed51c7b8dd64d13
Gyowanny Queiroz <gyowanny.queiroz@shopify.com>
2020-03-09 15:35:27 -0400

    Fix rss parser bug

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -8,8 +8,8 @@ class Fetcher
     open(feed.url) do |rss|
       fetched = RSS::Parser.parse(rss)
       fetched.items.map do |item|
-        title = fetched.is_a?(RSS::Atom::Feed) ? item.title.content : fetched.title
-        link = fetched.is_a?(RSS::Atom::Feed) ? item.link.href : fetched.link
+        title = fetched.is_a?(RSS::Atom::Feed) ? item.title.content : item.title
+        link = fetched.is_a?(RSS::Atom::Feed) ? item.link.href : item.link
         channel = fetched.is_a?(RSS::Atom::Feed) ? fetched.author.name.content : fetched.channel.title
 
         Headline.new(title, link, channel)
