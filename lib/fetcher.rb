1c147e36b4880073a63e472469cd280f04ce5325
Gyowanny Queiroz <gyowanny.queiroz@shopify.com>
2020-03-09 15:29:54 -0400

    Add support for Atom RSS type

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -1,13 +1,18 @@
 require 'headline'
 require 'rss'
 require 'open-uri'
+require 'rss/atom'
 
 class Fetcher
   def fetch(feed)
     open(feed.url) do |rss|
       fetched = RSS::Parser.parse(rss)
       fetched.items.map do |item|
-        Headline.new(item.title, item.link, fetched.channel.title)
+        title = fetched.is_a?(RSS::Atom::Feed) ? item.title.content : fetched.title
+        link = fetched.is_a?(RSS::Atom::Feed) ? item.link.href : fetched.link
+        channel = fetched.is_a?(RSS::Atom::Feed) ? fetched.author.name.content : fetched.channel.title
+
+        Headline.new(title, link, channel)
       end
     end
   end
