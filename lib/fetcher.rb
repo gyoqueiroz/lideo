bc7985ee5b9d22a19f31aa247916d43f4811a632
Gyowanny Queiroz <gyowanny.queiroz@shopify.com>
2020-03-12 17:42:57 -0400

    Add rescue block for failing feeds

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -5,15 +5,19 @@ require 'rss/atom'
 
 class Fetcher
   def fetch(feed)
-    open(feed.url) do |rss|
-      fetched = RSS::Parser.parse(rss)
-      fetched.items.map do |item|
-        title = fetched.is_a?(RSS::Atom::Feed) ? item.title.content : item.title
-        link = fetched.is_a?(RSS::Atom::Feed) ? item.link.href : item.link
-        channel = fetched.is_a?(RSS::Atom::Feed) ? fetched.author.name.content : fetched.channel.title
+    begin
+      open(feed.url) do |rss|
+        fetched = RSS::Parser.parse(rss)
+        fetched.items.map do |item|
+          title = fetched.is_a?(RSS::Atom::Feed) ? item.title.content : item.title
+          link = fetched.is_a?(RSS::Atom::Feed) ? item.link.href : item.link
+          channel = fetched.is_a?(RSS::Atom::Feed) ? fetched.author.name.content : fetched.channel.title
 
-        Headline.new(title, link, channel)
+          Headline.new(title, link, channel)
+        end
       end
+    rescue
+      puts "An error occurred while fetching #{feed.url}"
     end
   end
 end
